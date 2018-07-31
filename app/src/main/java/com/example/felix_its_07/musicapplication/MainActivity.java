d6c2e598c2dd76be7d8624d83ecb65c80ec484db
komal mane <komalmane48@gmail.com>
2018-07-31 16:52:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/felix_its_07/musicapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.felix_its_07.musicapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
